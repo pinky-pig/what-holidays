1f11fe5a8ee9928b263821a24f1812e103dc9427
pinky-pig <2545589993@qq.com>
2023-08-31 17:59:41 +0800

    fix: 面板展开动画

--- src/components/layout/HolidaysCalendar.vue
+++ src/components/layout/HolidaysCalendar.vue
@@ -4,15 +4,35 @@ const $container = ref<HTMLElement | null>()
 const isShow = ref(false)
 
 function setShow(flag: boolean) {
-
+  isShow.value = flag
 }
+
+watch(isShow, (value) => {
+  if (value) {
+    // 显示
+    $container.value!.style.transform = 'translateX(0px)'
+  }
+  else {
+    $container.value!.style.transform = 'translateX(1000px)'
+  }
+})
+
+defineExpose({
+  isShow,
+  setShow,
+})
 </script>
 
 <template>
   <div
     ref="$container"
+    class="absolute right-0 top-0 h-600px w-600px translate-x-full bg-gray-300 container"
   />
 </template>
 
 <style scoped>
+.container{
+  will-change: transform;
+  transition: all .5s linear;
+}
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+import HolidaysCalendar from '~/components/layout/HolidaysCalendar.vue'
 
+const $HolidaysCalendar = ref<typeof HolidaysCalendar | null>()
+
+let open = false
+function openHolidaysCalendar() {
+  open = !open
+  $HolidaysCalendar.value?.setShow(open)
+}
 </script>
 
 <template>
   <Background />
   <Suspense>
     <div class="fixed bottom-0 left-0 right-0 top-0 h-full w-full overflow-auto">
+      <button class="absolute left-0 top-0 z-10 btn" @click="openHolidaysCalendar">
+        open HolidaysCalendar
+      </button>
       <MainMap />
 
-      <HolidaysCalendar />
+      <HolidaysCalendar ref="$HolidaysCalendar" />
     </div>
   </Suspense>
 </template>
