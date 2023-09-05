e5193dc4967a395ccdddb9472472c3981cfe19a8
pinky-pig <2545589993@qq.com>
2023-09-05 21:52:59 +0800

    fix: searchInput

---
+++ src/components/ui/ScratchySearchInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="searchBar z-9 h30 w30 bg-red-100">
+    <div class="inner">
+      666
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.searchBar {
+  padding: 5px;
+  border-radius: 35px;
+}
+
+.searchBar::before {
+  content: '';
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  clip-path: polygon(46% 21%, 76% 31%, 65% 59%, 32% 40%);
+  background: url("http://species-in-pieces.com/img/textured-ui/green-texture.png") repeat center center/40px 40px;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,7 +4,9 @@
 <template>
   <div class="fixed bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden">
     <BorderBackground>
-      <MainMap class="pointer-events-auto" />
+      <div class="h-full w-full flex items-center justify-center">
+        <MainMap class="pointer-events-auto absolute left-0 top-0" />
+      </div>
     </BorderBackground>
 
     <RouterView />
