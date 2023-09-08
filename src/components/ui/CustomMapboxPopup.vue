2723a37de8f11e40f68d23676778664789540f7b
pinky-pig <2545589993@qq.com>
2023-09-08 23:32:25 +0800

    fix: customMapboxPopup styles

--- src/components/ui/CustomMapboxPopup.vue
+++ src/components/ui/CustomMapboxPopup.vue
@@ -1,32 +1,68 @@
 <script setup lang="ts">
 import Holidays from 'date-holidays'
+import dayjs from 'dayjs'
 import type { Holiday } from '~/types/holiday'
 
 const store = useAreaStore()
 let hd = null
 
 const isHolidayInNow = ref<false | Holiday[]>(false)
-// const holidayInMonth = ref<false | Holiday[]>(false)
+const holidayInMonth = ref<false | Holiday[]>(false)
 // 3. 最近的下個假日的信息（名字、時間）
-// const nextHoliday = ref<Holiday[] | null>(null)
+const nextHoliday = ref<null | Holiday >(null)
 
+onMounted(() => {
+  main()
+})
 watch(() => store.currentArea?.code, (v) => {
-  if (v) {
-    hd = new Holidays(v)
+  main()
+})
+
+// 主逻辑
+function main() {
+  if (store.currentArea?.code) {
+    hd = new Holidays(store.currentArea?.code)
+    const holidaysInYear = hd.getHolidays(2023)
 
     // 1. 今天這個地方是否是假日
-    isHolidayInNow.value = hd.isHoliday(new Date('2023-10-01'))
+    isHolidayInNow.value = hd.isHoliday(new Date('2023-01-01'))
     // 2. 這個月是否有假日，如果有，那麽信息（名字、時間）
+    holidayInMonth.value = getHolidaysInMonth(holidaysInYear)
+    // 3. 最近的下個假日的信息（名字、時間）
+    nextHoliday.value = getHolidayNextToDay(holidaysInYear)
   }
-})
+}
+
+function getHolidaysInMonth(holidaysInYear: Holiday[]): Holiday[] | [] {
+  const currentMonth = dayjs(new Date()).month()
+
+  // 过滤日期在当前月份的对象
+  const filteredArray = holidaysInYear.filter((item) => {
+    return dayjs(new Date(item.date)).month() === currentMonth
+  })
+
+  // 如果筛选结果为空，返回空数组，否则返回筛选后的结果
+  return filteredArray.length > 0 ? filteredArray : []
+}
+
+function getHolidayNextToDay(holidaysInYear: Holiday[]): Holiday | null {
+  const currentDate = new Date().getTime()
+
+  // 过滤日期在今天之后的对象
+  const filteredArray = holidaysInYear.filter((item) => {
+    const itemDate = new Date(item.date).getTime()
+    return itemDate > currentDate
+  })
+
+  // 如果找到了最近的日期，返回该对象，否则返回null
+  return filteredArray.length > 0 ? filteredArray[0] : null
+}
 </script>
 
 <template>
   <ScratchyBorder>
     <div class="popup">
-      <p>No holidays in now {{ isHolidayInNow ? isHolidayInNow[0].name : 'No holiday' }}</p>
-      <p>No holidays in month</p>
-      <p>Next holiday is New Year,it's time is 2024/01/01</p>
+      <p>今天是假日吗？{{ isHolidayInNow ? isHolidayInNow[0].name : '不是' }}</p>
     </div>
   </ScratchyBorder>
 </template>
@@ -36,6 +72,7 @@ watch(() => store.currentArea?.code, (v) => {
   width: 100%;
   height: 100%;
   padding: 5px;
+  background: #99CCF0;
   backdrop-filter: blur(10px);
 }
 </style>
